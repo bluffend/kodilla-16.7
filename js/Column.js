79f0ecf6b0a9be358ea16fc517f0881eaab098b8
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-25 13:09:57 +0200

    Add if else to column.js to prevent adding card without name

--- js/Column.js
+++ js/Column.js
@@ -20,29 +20,25 @@ function Column(id, name) {
 
 		columnAddCard.click(function (event) {
 			var cardName = prompt("Enter the name of the card");
-			event.preventDefault();
-			/* if (cardName === "" || name === null) {
-				alert("Please enter a name !")
+
+			if (cardName === "" || cardName === null) {
+				alert("Please give the card a name !")
 			}
 			else {
-				self.createCard(new Card(cardName))
+				event.preventDefault();
+				$.ajax({
+					url: baseUrl + '/card',
+					method: 'POST',
+					data: {
+						name: cardName,
+						bootcamp_kanban_column_id: self.id
+					},
+					success: function (response) {
+						var card = new Card(response.id, cardName);
+						self.createCard(card);
+					}
+				});
 			}
-			*/
-
-			$.ajax({
-				url: baseUrl + '/card',
-				method: 'POST',
-				data: {
-					name: cardName,
-					bootcamp_kanban_column_id: self.id
-				},
-				success: function (response) {
-					var card = new Card(response.id, cardName);
-					self.createCard(card);
-				}
-			});
-
-
 		});
 
 		// KONSTRUOWANIE ELEMENTU KOLUMNY
