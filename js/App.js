926f5362f7b62237cc0f8e4eff28e82faecd1e5d
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-25 23:07:34 +0200

    Add fixes to all 4 js files - to eliminate global variables

--- js/App.js
+++ js/App.js
@@ -1,34 +1,45 @@
 // OGÓLNA FUNKCJA
 
-var baseUrl = 'https://kodilla.com/pl/bootcamp-api'; 
-var myHeaders = {									
-	'X-Client-Id': '3481',
-	'X-Auth-Token': '5752ee80fe1289013f8c59b00b24ef12'
-};
 
-$.ajaxSetup({
-	headers: myHeaders
-});
 
-$.ajax({
-	url: baseUrl + '/board',
-	method: 'GET',
-	success: function (response) {
-		setupColumns(response.columns);
+function Start() {
+
+	var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+	var myHeaders = {
+		'X-Client-Id': '3481',
+		'X-Auth-Token': '5752ee80fe1289013f8c59b00b24ef12'
+	};
+
+	var board = initializeBoard(baseUrl);
+	var Card = initalizeCard(baseUrl);
+	var Column = initalizeColumn(baseUrl);
+
+	$.ajaxSetup({
+		headers: myHeaders
+	});
+
+	$.ajax({
+		url: baseUrl + '/board',
+		method: 'GET',
+		success: function (response) {
+			setupColumns(response.columns);
+		}
+	});
+
+	function setupColumns(columns) {
+		columns.forEach(function (column) {
+			var col = new Column(column.id, column.name);
+			board.createColumn(col);
+			setupCards(col, column.cards);
+		});
 	}
-});
 
-function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
-		board.createColumn(col);
-		setupCards(col, column.cards);
-    });
+	function setupCards(col, cards) {
+		cards.forEach(function (card) {
+			var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+			col.createCard(cardObj);
+		})
+	}
 }
 
-function setupCards(col, cards) {
-	cards.forEach(function (card) {
-        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    	col.createCard(cardObj);
-  	})
-}
+Start ();
--- js/Board.js
+++ js/Board.js
@@ -1,37 +1,48 @@
-var board = {
-	name: 'Tablica Kanban',
-	createColumn: function (column) {
-		this.element.append(column.element);
-		initSortable();
-	},
-	element: $('#board .column-container')
-};
+function initializeBoard(baseUrl) {
+	var board = {
+		name: 'Tablica Kanban',
+		createColumn: function (column) {
+			this.element.append(column.element);
+			initSortable();
+		},
+		element: $('#board .column-container')
+	};
 
-$('.create-column')
-	.click(function () {
-		var columnName = prompt('Enter a column name');
+	var Column = initalizeColumn(baseUrl);
 
-		if (columnName === "" || columnName === null) {
-			alert("Please give the column a name !")
-		}
-		else {
-			$.ajax({
-				url: baseUrl + '/column',
-				method: 'POST',
-				data: {
-					name: columnName
-				},
-				success: function (response) {
-					var column = new Column(response.id, columnName);
-					board.createColumn(column);
+	$('.create-column')
+		.click(function () {
+			var inputColumnName = prompt('Enter a column name');
+
+			if (inputColumnName === null) { // klinięcie "anuluj"
+				return;
+			}
+			else {
+				if (inputColumnName === "") {
+					columnName = "No name";
 				}
-			});
-		}
-	});
+				else {
+					columnName = inputColumnName;
+				}
+				$.ajax({
+					url: baseUrl + '/column',
+					method: 'POST',
+					data: {
+						name: columnName
+					},
+					success: function (response) {
+						var column = new Column(response.id, columnName);
+						board.createColumn(column);
+					}
+				});
+			}
+		});
 
-function initSortable() {
-	$('.card-list').sortable({
-		connectWith: '.card-list',
-		placeholder: 'card-placeholder'
-	}).disableSelection();
-}
+	function initSortable() {
+		$('.card-list').sortable({
+			connectWith: '.card-list',
+			placeholder: 'card-placeholder'
+		}).disableSelection();
+	}
+	return board;
+} 
